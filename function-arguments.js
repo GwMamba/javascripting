0e785e2b5d302f62dc7608db42e26fceb0142b47
Gerald <gerald.mwang@gmail.com>
2023-08-13 20:05:03 -0600

    My work on javascripting so far

---
+++ function-arguments.js
@@ -0,0 +1,6 @@
+function math (x, y, z) {
+var mult = y * z;
+var total = mult + x;
+return total;
+}
+console.log(math(53, 61, 67));
---
+++ rounding-numbers.js
@@ -0,0 +1,4 @@
+let roundUp = 1.5;
+Math.round(roundUp);
+
+console.log(Math.round(roundUp));
